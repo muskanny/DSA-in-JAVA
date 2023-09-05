1867a35b00f46097015be9ae12e32587b844b4bb
muskan singh <muskan.s.del@gmail.com>
2023-09-06 04:54:52 +0530

    Create insertion_beg.java

---
+++ Linked_Lists/insertion_beg.java
@@ -0,0 +1,10 @@
+package Linked_Lists;
+import java.util.*;
+import java.io.*;
+// insertion at the beginning
+public class insertion_beg{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
