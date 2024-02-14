79664518b2ce21ca1c11ed3f2001c7a55738ef9e
muskan singh <muskan.s.del@gmail.com>
2024-02-14 18:11:00 +0530

    dsa

---
+++ queues/stacksusingqueue.java
@@ -0,0 +1,46 @@
+class stackusingqueue {
+    Queue<Integer> q1=new LinkedList<>();
+    Queue<Integer> q2=new LinkedList<>();
+    
+    public MyStack() {
+     
+
+    }
+    
+    public void push(int x) {
+        q2.add(x);
+        while(!q1.isEmpty()){
+            q2.add(q1.remove());
+        }
+
+        q1=q2;
+        q2=new LinkedList<>();
+    }
+    
+    public int pop() {
+        if(q1.isEmpty()){
+            return -1;
+        }
+
+        else{
+            return q1.remove();
+        }
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
---
+++ stacks/minstack.java
@@ -0,0 +1,57 @@
+
+//brute force method
+class Mminstack {
+    Stack<Integer> st=new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        st.push(val);
+    }
+    
+    public void pop() {
+        if(st.isEmpty()){
+            return;
+        }
+        st.pop();
+    }
+    
+    public int top() {
+        if(st.isEmpty()){
+            return -1;
+        }
+        return st.peek();
+    }
+    
+    public int getMin() {
+        if(st.isEmpty()){
+            return -1;
+        }
+        Stack<Integer> temp=new Stack<>();
+        int min=Integer.MAX_VALUE;
+        while(!st.isEmpty()){
+            
+            int current=st.pop();
+            temp.push(current);
+            if(current<min){
+                min=current;
+            }
+        }
+
+        while(!temp.isEmpty()){
+            st.push(temp.pop());
+        }
+
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
